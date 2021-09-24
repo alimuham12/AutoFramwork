b5a2f658b0507e2c8304a0899cbcac4b2266795a
alimuham12 <muhammad.ali@indusvalleylabs.com>
2021-09-24 16:15:55 +0500

    New variable declared

--- src/main/java/pageObjects/HomePageElements.java
+++ src/main/java/pageObjects/HomePageElements.java
@@ -2,6 +2,7 @@ package main.java.pageObjects;
 
 public interface HomePageElements {
     String signInButton = "body > app-root > po-header > p-menubar > div > div.p-menubar-end.ng-star-inserted > div > button > span";
+    String alphe = "This";
 
 
 }
